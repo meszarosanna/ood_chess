6288ec0dcfd46622a73bf240e16464786e06c5ef
meszarosanna <m.meszaros.anna@gmail.com>
2025-07-31 11:39:16 +0100

    Added legal: bool option to the natrual engines

--- searchless_chess/src/engines/neural_engines.py
+++ searchless_chess/src/engines/neural_engines.py
@@ -70,54 +70,76 @@ def _update_scores_with_repetitions(
 class ActionValueEngine(NeuralEngine):
   """Neural engine using a function P(r | s, a)."""
 
-  def analyse(self, board: chess.Board) -> engine.AnalysisResult:
+  def analyse(self, board: chess.Board, legal: bool = True) -> engine.AnalysisResult:
     """Returns buckets log-probs for each action, and FEN."""
-    # Tokenize the legal actions.
-    sorted_legal_moves = engine.get_ordered_legal_moves(board)
-    legal_actions = [utils.MOVE_TO_ACTION[x.uci()] for x in sorted_legal_moves]
-    legal_actions = np.array(legal_actions, dtype=np.int32)
-    legal_actions = np.expand_dims(legal_actions, axis=-1)
-    # Tokenize the return buckets.
-    #legal_dummy_return_buckets = np.zeros((len(legal_actions), 1), dtype=np.int32)
-    # Tokenize the board.
-    tokenized_fen = tokenizer.tokenize(board.fen()).astype(np.int32)
-    #legal_sequences = np.stack([tokenized_fen] * len(legal_actions))
-    # Create the sequences.
-    #legal_sequences = np.concatenate(
-    #    [legal_sequences, legal_actions, legal_dummy_return_buckets],
-    #    axis=1,
-    #)
+    if legal:
+      # Get the log-probs for all legal actions.
 
-    #Tokenize all actions
-    all_actions = list(range(len(utils.ACTION_TO_MOVE)))
-    all_actions = np.array(all_actions, dtype=np.int32)
-    all_actions = np.expand_dims(all_actions, axis=-1)
+      # Tokenize the legal actions.
+      sorted_legal_moves = engine.get_ordered_legal_moves(board)
+      legal_actions = [utils.MOVE_TO_ACTION[x.uci()] for x in sorted_legal_moves]
+      legal_actions = np.array(legal_actions, dtype=np.int32)
+      legal_actions = np.expand_dims(legal_actions, axis=-1)
+      tokenized_fen = tokenizer.tokenize(board.fen()).astype(np.int32)
 
-    all_dummy_return_buckets = np.zeros((len(all_actions), 1), dtype=np.int32)
-    sequences = np.stack([tokenized_fen] * len(all_actions))
-    sequences = np.concatenate(
-        [sequences, all_actions, all_dummy_return_buckets],
-        axis=1,
-    )
-    total_action_log_probs = self.predict_fn(sequences)[:, -1]
 
-    legal_actions = np.squeeze(legal_actions)
-    action_log_probs = total_action_log_probs[legal_actions]
-    assert len(action_log_probs) == len(list(board.legal_moves))
-    return {'total_log_probs': total_action_log_probs, 'legal_log_probs': action_log_probs, 'fen': board.fen()}
+      dummy_return_buckets = np.zeros((len(legal_actions), 1), dtype=np.int32)
+      sequences = np.stack([tokenized_fen] * len(legal_actions))
+      sequences = np.concatenate(
+          [sequences, legal_actions, dummy_return_buckets],
+          axis=1,
+      )
+      legal_log_probs = self.predict_fn(sequences)[:, -1]
+      assert len(legal_log_probs) == len(list(board.legal_moves))
+      return legal_log_probs
+    
+    else:
+      # Get the log-probs for all actions.
+      #Tokenize all actions
+      all_actions = list(range(len(utils.ACTION_TO_MOVE)))
+      all_actions = np.array(all_actions, dtype=np.int32)
+      all_actions = np.expand_dims(all_actions, axis=-1)
+      tokenized_fen = tokenizer.tokenize(board.fen()).astype(np.int32)
 
-  def play(self, board: chess.Board) -> chess.Move:
-    return_buckets_log_probs = self.analyse(board)['legal_log_probs']
+
+      all_dummy_return_buckets = np.zeros((len(all_actions), 1), dtype=np.int32)
+      sequences = np.stack([tokenized_fen] * len(all_actions))
+      sequences = np.concatenate(
+          [sequences, all_actions, all_dummy_return_buckets],
+          axis=1,
+      )
+      total_log_probs = self.predict_fn(sequences)[:, -1]
+
+      #legal_actions = legal_actions.flatten()
+      #action_log_probs = total_action_log_probs[legal_actions]
+      #return {'total_log_probs': total_action_log_probs, 'legal_log_probs': action_log_probs, 'fen': board.fen()}
+      return total_log_probs
+
+
+  def play(self, board: chess.Board, legal: bool = True) -> chess.Move:
+    return_buckets_log_probs = self.analyse(board, legal)
     return_buckets_probs = np.exp(return_buckets_log_probs)
     win_probs = np.inner(return_buckets_probs, self._return_buckets_values)
-    _update_scores_with_repetitions(board, win_probs)
-    sorted_legal_moves = engine.get_ordered_legal_moves(board)
-    if self.temperature is not None:
-      probs = scipy.special.softmax(win_probs / self.temperature, axis=-1)
-      return self._rng.choice(sorted_legal_moves, p=probs)
+
+    if legal:
+      _update_scores_with_repetitions(board, win_probs) #ONLY FOR LEGAL MOVES
+
+      sorted_legal_moves = engine.get_ordered_legal_moves(board)
+      if self.temperature is not None:
+        probs = scipy.special.softmax(win_probs / self.temperature, axis=-1)
+        return self._rng.choice(sorted_legal_moves, p=probs)
+      else:
+        best_index = np.argmax(win_probs)
+        return sorted_legal_moves[best_index]
     else:
-      best_index = np.argmax(win_probs)
-      return sorted_legal_moves[best_index]
+      all_moves = [chess.Move.from_uci(utils.ACTION_TO_MOVE[i]) for i in range(len(utils.ACTION_TO_MOVE))]
+      if self.temperature is not None:
+        probs = scipy.special.softmax(win_probs / self.temperature, axis=-1)
+        return self._rng.choice(all_moves, p=probs)
+      else:
+        best_index = np.argmax(win_probs)
+        return all_moves[best_index] 
+
 
 
 class StateValueEngine(NeuralEngine):
@@ -175,7 +197,7 @@ class StateValueEngine(NeuralEngine):
 class BCEngine(NeuralEngine):
   """Defines a policy that predicts action probs."""
 
-  def analyse(self, board: chess.Board) -> engine.AnalysisResult:
+  def analyse(self, board: chess.Board, legal: bool = False) -> engine.AnalysisResult:
     """Defines a policy that predicts action probs."""
     tokenized_fen = tokenizer.tokenize(board.fen()).astype(np.int32)
     tokenized_fen = np.expand_dims(tokenized_fen, axis=0)
@@ -183,28 +205,41 @@ class BCEngine(NeuralEngine):
     sequences = np.concatenate([tokenized_fen, dummy_actions], axis=1)
     total_action_log_probs = self.predict_fn(sequences)[0, -1]
     assert len(total_action_log_probs) == utils.NUM_ACTIONS
-
-    #We must renormalize the output distribution to only the legal moves.
-    sorted_legal_moves = engine.get_ordered_legal_moves(board)
-    legal_actions = [utils.MOVE_TO_ACTION[x.uci()] for x in sorted_legal_moves]
-    legal_actions = np.array(legal_actions, dtype=np.int32)
-    action_log_probs = total_action_log_probs[legal_actions]
-    action_log_probs = jnn.log_softmax(action_log_probs)
-    assert len(action_log_probs) == len(list(board.legal_moves))
-    return {'total_log_probs': total_action_log_probs, 'legal_log_probs' : action_log_probs, 'fen': board.fen()}
-
-  def play(self, board: chess.Board) -> chess.Move:
-    action_log_probs = self.analyse(board)['total_log_probs']
-    all_moves = [chess.Move.from_uci(utils.ACTION_TO_MOVE[i]) for i in range(len(utils.ACTION_TO_MOVE))]
-    sorted_legal_moves = engine.get_ordered_legal_moves(board)
-    if self.temperature is not None:
-      probs = scipy.special.softmax(
-          action_log_probs / self.temperature, axis=-1
-      )
-      return self._rng.choice(all_moves, p=probs)
+    if legal: 
+      #We must renormalize the output distribution to only the legal moves.
+      sorted_legal_moves = engine.get_ordered_legal_moves(board)
+      legal_actions = [utils.MOVE_TO_ACTION[x.uci()] for x in sorted_legal_moves]
+      legal_actions = np.array(legal_actions, dtype=np.int32)
+      action_log_probs = total_action_log_probs[legal_actions]
+      action_log_probs = jnn.log_softmax(action_log_probs)
+      assert len(action_log_probs) == len(list(board.legal_moves))
+      return action_log_probs
     else:
-      best_index = np.argmax(action_log_probs)
-      return all_moves[best_index]
+      return total_action_log_probs
+
+  def play(self, board: chess.Board, legal: bool = False) -> chess.Move:
+    if legal:
+      action_log_probs = self.analyse(board, legal)
+      sorted_legal_moves = engine.get_ordered_legal_moves(board)
+      if self.temperature is not None:
+        probs = scipy.special.softmax(
+            action_log_probs / self.temperature, axis=-1
+        )
+        return self._rng.choice(sorted_legal_moves, p=probs)
+      else:
+        best_index = np.argmax(action_log_probs)
+        return sorted_legal_moves[best_index]
+    else:
+      action_log_probs = self.analyse(board, legal)
+      all_moves = [chess.Move.from_uci(utils.ACTION_TO_MOVE[i]) for i in range(len(utils.ACTION_TO_MOVE))]
+      if self.temperature is not None:
+        probs = scipy.special.softmax(
+            action_log_probs / self.temperature, axis=-1
+        )
+        return self._rng.choice(all_moves, p=probs)
+      else:
+        best_index = np.argmax(action_log_probs)
+        return all_moves[best_index]
 
 
 def wrap_predict_fn(
