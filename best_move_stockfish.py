88902773d8c06b9e4c038fc9329fdd34c3da40e2
meszarosanna <m.meszaros.anna@gmail.com>
2025-09-10 09:24:49 +0100

    best move stockfish update to depth and import time

--- best_move_stockfish.py
+++ best_move_stockfish.py
@@ -2,8 +2,9 @@
 
 import chess
 import chess.engine
+import time
 
-def get_best_move(fen_string, depth, time, multipv, win_probab):
+def get_best_move(fen_string, depth, stime, multipv, win_probab):
     """Get the best move for a given FEN position."""
     
     # Create a board from the FEN string
@@ -11,7 +12,9 @@ def get_best_move(fen_string, depth, time, multipv, win_probab):
 
         
     # Initialize Stockfish engine
-    with chess.engine.SimpleEngine.popen_uci("./Stockfish/src/stockfish") as engine:
+    with chess.engine.SimpleEngine.popen_uci("/home/am3049/ood_chess/Stockfish/src/stockfish") as engine:
+        engine.configure({"Threads": 1})
+
         if win_probab:
             # Configure engine to show WDL
             engine.configure({"UCI_ShowWDL": "true"})
@@ -64,7 +67,10 @@ def get_best_move(fen_string, depth, time, multipv, win_probab):
                 print(f"{move}\t{move_data['win_pct']:.1f}%\t{move_data['draw_pct']:.1f}%\t{move_data['loss_pct']:.1f}%\t{score_str}")
         else:
             # Get top multipv moves
-            result = engine.analyse(board, chess.engine.Limit(depth=depth, time=time), multipv=multipv)
+            start = time.time()
+            result = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=multipv)
+            end = time.time()
+            print(f"Analysis time for top {multipv} moves: {end - start:.2f} seconds")
             #score = [result[i]["score"] for i in range(multipv)]
             best_move = [result[i]["pv"][0].uci() for i in range(multipv)] # result is a dictionary with keys "pv": list of moves best moves; "score": (centipawns) evaluation of the position; "depth": how deep the engine searched            
             # Print the best move in algebraic notation
@@ -74,16 +80,21 @@ def get_best_move(fen_string, depth, time, multipv, win_probab):
 
 if __name__ == "__main__":
     # The FEN string for the position you want to analyze
-    fen = "rnbqkbnr/pppppppp/7r/8/8/7Q/PPPPPPPP/RNBQKBNN b" # KQkq - 0 1"  # Starting position
+    #fen = "rnbqkbnr/pppppppp/7r/8/8/7Q/PPPPPPPP/RNBQKBNN b" # KQkq - 0 1"  # Starting position
     #fen = "rnbqkbnr/pppppppp/8/8/8/8/QQQQQQQQ/RNBQKBNR w" # KQkq - 0 1"  # Starting position
     #fen = "rnkqkbnr/pppppQpp/8/8/8/8/PPPPPQPP/RNBQKBNR b"
-    
-    
-    depth = None      # None means unlimited depth
-    time = 0.5       # Time limit in seconds (originally, it was 0.05)
-    multipv = 5    # Number of best moves to return
-    time *= multipv
-    win_probab = True # !!!Does not always reflect well the quality of the move
+    #fen = "R7/5N1k/R7/2N5/8/8/6N1/6R1" #f7g5 
+    #fen = "rnbpkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    fen  = "r5k1/1pp2Bp1/5n1p/1q2N3/3P4/7P/5PP1/4Q1K1 b - - 2 30"
 
-    best_move = get_best_move(fen, depth, time, multipv, win_probab) 
+    
+    
+    depth = 30      # None means unlimited depth
+    stime = 0.5       # Time limit in seconds (originally, it was 0.05)
+    multipv = 1    # Number of best moves to return
+    stime *= multipv
+    win_probab = False # !!!Does not always reflect well the quality of the move
+
+    best_move = get_best_move(fen, depth, stime, multipv, win_probab) 
     print(f"Best move: {best_move}")
+    
