7085088ce327498920669a54c31de99613d2dd1c
meszarosanna <m.meszaros.anna@gmail.com>
2025-10-15 11:32:54 +0100

    Copyright added

--- searchless_chess/checkpoints/download.sh
+++ searchless_chess/checkpoints/download.sh
@@ -1,3 +1,9 @@
+# Copyright Meszaros et al.
+#
+# This script downloads the pretrained model on the filtered dataset.
+# For simplicity, the file is in the searchless_chess folder, but it was created by Meszaros et al.
+#
+
 mkdir -p local/behavioral_cloning
 cd local/behavioral_cloning
 gdown --folder https://drive.google.com/drive/folders/1X-LUMs2Zv8OpbX5tlfjgfMnFkKJW9S-r?usp=sharing
